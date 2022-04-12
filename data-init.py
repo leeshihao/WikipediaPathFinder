c691f21232e4af44ea5fa3189755bfdeea7aedb6
ethanseow <eseow6088@bths.edu>
2022-04-11 23:37:10 -0400

    added first file

---
+++ data-init.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+from email.policy import default
+import requests
+def getPageIdFromTitle(title):
+    try:
+        apiReqUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles={}'.format(title)
+        r = requests.get(apiReqUrl)
+        return list(r.json()['query']['pages'].keys())[0]
+    except:
+        print('Title does not exist')
+        return None
+
+
+def getAllPageLinks(id):
+    try:
+        apiReqUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&pageids={}&generator=linkshere&formatversion=2&glhprop=title&glhlimit=20'.format(id)
+        r = requests.get(apiReqUrl)
+        return r.json()['query']['pages']
+    except:
+        print('Error, id does not exist to wikipedia page')
+        return None
+def getNodesFromPages(pages):
+    if not pages:
+        print('Pages is not an object')
+        return None
+    nodes = defaultdict(dict)
+    for page in pages:
+        nodes[(page['pageid'])] = {}
+    return nodes
+
+def generateGraph(title):
+    startId = getPageIdFromTitle(title)
+    pages = getAllPageLinks(startId)
+    graph = getNodesFromPages(pages)
+    MAX_LEVEL = 2
+    def addNodes(level,graphNodes):
+        if level >= MAX_LEVEL:
+            return
+        for node in graphNodes:
+            graphNodes[node] = getNodesFromPages(getAllPageLinks(node))
+            addNodes(level+1,graphNodes[node])
+    addNodes(0,graph)
+    return graph
+
+
+
+startUrl = 'Stanford University'
+endUrl = 'Harvard University'
+startId = getPageIdFromTitle(startUrl)
+endId = getPageIdFromTitle(endUrl)
+print(endId)
+visitedSet = set()
+
+generateGraph(startUrl)
+
