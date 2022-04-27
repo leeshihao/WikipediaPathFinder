39a0180f5ae3c28d636c4671324904a391c19f6a
ethanseow <eseow6088@bths.edu>
2022-04-27 18:41:13 -0400

    created graph and node class

--- graph.py
+++ graph.py
@@ -1,9 +1,46 @@
 import requests
 import re
-startArticle = "https://en.wikipedia.org/wiki/Stanford_University"
-endArticle = "https://en.wikipedia.org/wiki/Samsung"
+startArticle = "Stanford_University"
 
-r = requests.get(startArticle)
-content = str(r.content)
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.connected_nodes = {}
+    def add_node(self,node):
+        if node.val in self.connected_nodes:
+            return None
+        self.connected_nodes[node.val] = node
+        return node
+        
+def create_graph(start_node, layers, nodes_per_layer):
+    
+    layers -= 1
+    if layers == 0:
+        return start_node
 
-print(set(re.findall("\"/wiki/([^\":]*)\"",content)))
+    url = 'https://en.wikipedia.org/wiki/' + start_node.val
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e,'\nThe url connected with the error:' ,url)
+        return None
+    else:
+        content = str(res.content)
+        links = set(re.findall("\"/wiki/([^\":]*)\"",content))
+
+        max_num_links = nodes_per_layer
+
+        for link in links:
+            if max_num_links == 0:
+                break
+            new_node = Node(link)
+            # print('Layer{}: {}'.format(layers,link))
+            start_node.add_node(create_graph(new_node,layers,nodes_per_layer))
+            max_num_links -= 1
+        return start_node
+
+start_node = Node(startArticle)
+
+graph = create_graph(start_node,3,10)
+print(graph.connected_nodes)
