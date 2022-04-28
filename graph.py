388be75a0d2317c9fc9d406e922b76495c71b3c1
ethanseow <eseow6088@bths.edu>
2022-04-27 23:58:34 -0400

    fixed issue with BFS path finder

--- graph.py
+++ graph.py
@@ -11,6 +11,44 @@ class Node:
             return None
         self.connected_nodes[node.val] = node
         return node
+    def BFS_Dest_Exists(self,dest):
+        visited = set(self.val)
+        q = [self.connected_nodes]
+        while q:
+            connected_nodes = q.pop()
+            for key in connected_nodes:
+                node = connected_nodes[key]
+                if node.val == dest:
+                    return True
+                if node.val in visited:
+                    continue
+                q.append(node.connected_nodes)
+                visited.add(node.val)
+        return False 
+    def BFS_Find_Path(self,dest):
+        if not self.BFS_Dest_Exists(dest):
+            return None
+        visited = set([self.val])
+        q = [self.connected_nodes]
+        chain = [[self.val]]
+        chain_parent = -1
+        while q:
+            connected_nodes = q.pop(0)
+            chain_parent += 1
+            for key in connected_nodes:
+                node = connected_nodes[key]
+                if node.val == dest:
+                    print(chain[chain_parent])
+                    return True
+                if node.val in visited:
+                    continue
+                q.append(node.connected_nodes)
+                curr_chain = (chain[chain_parent])[::]
+                curr_chain.append(node.val)
+                chain.append(curr_chain)
+                visited.add(node.val)
+        return False 
+            
         
 def create_graph(start_node, layers, nodes_per_layer):
     
@@ -28,7 +66,7 @@ def create_graph(start_node, layers, nodes_per_layer):
         return Node('deadend')
     else:
         content = str(res.content)
-        links = set(re.findall("\"/wiki/([^\":]*)\"",content))
+        links = list(re.findall("\"/wiki/([^\":]*)\"",content))
 
         max_num_links = nodes_per_layer
 
@@ -36,12 +74,14 @@ def create_graph(start_node, layers, nodes_per_layer):
             if max_num_links == 0:
                 break
             new_node = Node(link)
-            # print('Layer{}: {}'.format(layers,link))
+            print('Layer{}: {}'.format(layers,link))
             start_node.add_node(create_graph(new_node,layers,nodes_per_layer))
             max_num_links -= 1
         return start_node
 
 start_node = Node(startArticle)
 
-graph = create_graph(start_node,3,10)
+graph = create_graph(start_node,5,2)
 print(graph.val)
+
+graph.BFS_Find_Path('Language')
